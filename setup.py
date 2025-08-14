a1fb82053873e314db34c340fcff7eb65900a4cb
PKwhiting <60897387+PKwhiting@users.noreply.github.com>
2025-08-14 20:57:11 +0000

    package name change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="shopify-graphql-sdk",
+    name="Shopify",
     version="1.0.0",
     author="PKwhiting",
     author_email="",
